61aadeb59ecdfe77e3efc3d6f0cfc2ba3febda9f
Piumi Abeynayaka <piumi.abeynayaka@mail.mcgill.ca>
2016-02-15 15:24:00 -0500

    Changed from bool method to subset formula for question 5. Completed A1.

--- assignments/Assignment1.R
+++ assignments/Assignment1.R
@@ -1,19 +1,25 @@
 ## Piumi Abeynayaka ID 260458806
 
-## Question 1
+## ====================================================================
+## QUESTION 1
 
 ## i)
-x <- sample(1:100, size=1)
-y <- sample(1:100, size=1)
+(x <- sample(1:100, size=1))
+(y <- sample(1:100, size=1))
 x1 <- x
 x <- y
 y <- x1
+(x);(y)
+
+
 
 ## ii)
 ## M is a vector of randomly sampled numbers from 1 to 10000.
 ## The size of M (ie. the number of numbers in it) is also 
-## randomly chosen from 1 to 10000.
-M <- sample(1:10000,size=sample(1:10000,size=1))
+## randomly chosen from  a number between 1 to 10000.
+M <- sample(1:10000, size=sample(1:10000, size=1))
+
+
 
 ## iii)
 maxVal <- M[1]
@@ -23,6 +29,8 @@ for (i in 2:length(M)){
   }
 }
 maxVal
+
+
 
 ## iv)
 findMax <- function(myData){
@@ -35,23 +43,25 @@ findMax <- function(myData){
   return(maxVal)
 }
 
-
-## Question 2 
+## ====================================================================
+## QUESTION 2
 
 ## i)
 n <- sample(1:20, size=1)
 m <- sample(1:20, size=1)
-P <- matrix(nrow=n, ncol=m)
+M <- matrix(nrow=n, ncol=m)
+
+
 
 ## ii)
-smallerDim <- ifelse(ncol(M)<=nrow(M), yes = ncol(M), no = nrow(M))
+smallerDim <- ifelse((ncol(M)<=nrow(M)), yes = ncol(M), no = nrow(M))
 for (i in 1:smallerDim){
   M[i,i] <- 0
 }
 
 
-## iii) and iv)
 
+## iii) and iv)
 for (i in 1:nrow(M)){
   for (j in 1:ncol(M)){
     if (i<j){
@@ -80,24 +90,33 @@ initializeM <- function (M){
   }
   return (M)
 }
-## Question 3
+
+## ====================================================================
+## QUESTION 3
 
 ## i)
 q1 <- as.list(setdiff(letters,c("a","e","i","o","u")))
 
+
+
 ## ii)
 q2 <- as.list(seq(from=1, to=100, by=2))
+
+
 
 ## iii)
 q3 <- list(c(09,08,92),c(05,01,86))
 
 myList <- list(q1,q2,q3)
 
+
+
 ## iv)
-sub.myList <- list(myList[[1]], myList[[3]])
+myList[c(1,3)]
 
+## ====================================================================
+## QUESTION 4
 
-## Question 4
 setwd("~/repos/comp364")
 source("~/repos/comp364/src/hucMini.R")
 dataset.collection <- c("miniTCGA", "nki", "vanvliet")
@@ -107,136 +126,123 @@ for (dataset in dataset.collection){
   print(names(huc[[dataset]]))
 }
 
-## Question 5
+## ====================================================================
+## QUESTION 5
 
 ## Data frame for answers to Question 5
 Q5 <- data.frame()
 
-## a) Calculate number of patients
-num.patients <- function (myData){
-  return (length(huc[[myData]][["clinical"]][["id"]]))
-}
+## a) CALCULATE NUMBER OF PATIENTS
 
 number.of.patients <- c()
 for (dataset in dataset.collection){
-  number.of.patients <- c(number.of.patients,(num.patients(dataset)))
+  number.of.patients <- c(number.of.patients,
+                          length(huc[[dataset]][["clinical"]][["id"]]))
 }
 
-Q5 <- rbind(c(number.of.patients,NA))
+number.of.patients[length(dataset.collection)+1] <- sum(number.of.patients)
 
-## b) ER+ fraction in each dataset
-er.plus <- function (myData){
-  return (sum(huc[[myData]][["clinical"]][["er"]], na.rm=TRUE))
-}
+Q5 <- rbind(number.of.patients)
 
-fraction.er.plus <- c()
+
+
+## b) ER+ FRACTION IN EACH DATASET
+## AND
+## d) ER+ FRACTION IN ALL DATASETS
+
+er.plus <- c()
 for (dataset in dataset.collection){
-  fraction.er.plus <- c(fraction.er.plus,(er.plus(dataset)/num.patients(dataset)))
+  er.plus <- c(er.plus, length(subset(huc[[dataset]][["clinical"]], er)[,1]))
 }
 
-Q5 <- rbind(Q5,c(fraction.er.plus,NA))
+er.plus[length(dataset.collection)+1] <- sum(er.plus)
 
-## c) good to poor outcomes ratio in each dataset
-good.poor.ratio <- function (myData){
-  poor <- sum(huc[[myData]][["clinical"]][["event.5"]], na.rm=TRUE)
-  na.values <- sum(is.na(huc[[myData]][["clinical"]][["event.5"]]))
-  good <- num.patients(myData)-na.values-poor
-  paste(good,":",poor, sep="")
-}
+Q5 <- rbind(Q5,er.plus/number.of.patients)
 
-good.poor.outcomes <- c()
+
+
+## c) GOOD TO POOR OUTCOMES RATIO FOR EACH DATASET
+
+good.poor.string <- c()
+good.poor.numeric <- c()
 for (dataset in dataset.collection){
-  good.poor.outcomes <- c(good.poor.outcomes,good.poor.ratio(dataset))
+  good <- length(subset(huc[[dataset]][["clinical"]],!event.5)[,1])
+  poor <- length(subset(huc[[dataset]][["clinical"]],event.5)[,1])
+  good.poor.string <- c(good.poor.string, paste(good,":",poor, sep=""))
+  good.poor.numeric <- c(good.poor.numeric, good/poor)
 }
 
-Q5 <- rbind(Q5,c(good.poor.outcomes,NA))
+Q5 <- rbind(Q5,c(good.poor.string,NA))
+Q5 <- rbind(Q5,c(good.poor.numeric,NA))
 
-## d) ER+ fraction for all patients
-query <- function (myData, var, patient){
-  ## Returns the value found in a given sub-object of a given dataset
-  ## for a given patient.
-  ##
-  ## myData: a string matching one of the datasets
-  ## var: a string matching the name of a sub-object
-  ## patient: an integer, greater than 0,
-  ##          equal to or less than the total number of patients in the dataset
-  return (huc[[myData]][["clinical"]][[var]][patient])
-}
 
-bool <- c()
+
+## e) HER2+ FRACTION
+
+her2 <- c()
 for (dataset in dataset.collection){
-  for (a in 1:num.patients(dataset)){
-    bool <- c(bool, (query(dataset,"er",a)))
-  }
+  her2 <- c(her2,length(subset(huc[[dataset]][["clinical"]],her2)[,1]))
 }
 
-ER.plus.fraction <- sum(bool, na.rm = TRUE)/length(bool)
+her2[length(dataset.collection)+1] <- sum(her2)
 
-Q5[1,4] <- length(bool)
-Q5[2,4] <- ER.plus.fraction
+Q5 <- rbind(Q5,her2/number.of.patients)
 
-## e) HER2+ fraction
-bool <- c()
+
+
+## f) ER+ & HER2+
+
+er.and.her2 <- c()
 for (dataset in dataset.collection){
-  for (a in 1:num.patients(dataset)){
-    bool <- c(bool, (query(dataset,"her2",a)))
-  }
+  er.and.her2 <- c(er.and.her2,
+                   length(subset(huc[[dataset]][["clinical"]],
+                                 (her2 & er))[,1])
+                   )
 }
 
-HER2.plus.fraction <- sum(bool, na.rm = TRUE)/length(bool)
+er.and.her2[length(dataset.collection)+1] <- sum(er.and.her2)
 
-Q5 <- rbind(Q5,c(rep(NA,times = length(dataset.collection)),HER2.plus.fraction))
+Q5 <- rbind(Q5,er.and.her2/number.of.patients)
 
-## f) ER+ and HER2+
-bool <- c()
+
+
+## g) ER- & HER2- & LYMPH+ & <50 YEARS OLD
+
+er.her.lymph.age <- c()
 for (dataset in dataset.collection){
-  for (a in 1:num.patients(dataset)){
-    bool <- c(bool, (query(dataset,"er",a) 
-                     & query(dataset,"her2",a)))
-  }
+  er.her.lymph.age <- c(er.her.lymph.age,
+                        length(subset(huc[[dataset]][["clinical"]],
+                                      (!er & !her2 & lymph & age<50))[,1])
+                        )
 }
 
-ER.and.HER2 <- sum(bool, na.rm = TRUE)/length(bool)
+er.her.lymph.age[length(dataset.collection)+1] <- sum(er.her.lymph.age)
 
-Q5 <- rbind(Q5,c(rep(NA,times = length(dataset.collection)),ER.and.HER2))
+Q5 <- rbind(Q5,er.her.lymph.age/number.of.patients)
 
-## g) ER- and HER2- and lymph+ and <50 years old
-bool <- 0
+
+
+## h) ER- & HER2- & LYMPH+ & <50 YEARS OLD & NO EVENT AT 5 YEARS
+
+er.her.lymph.age.event <- c()
 for (dataset in dataset.collection){
-  for (a in 1:num.patients(dataset)){
-      bool <- c(bool, (!query(dataset,"er",a) 
-                        & !query(dataset,"her2",a) 
-                        & query(dataset,"lymph",a) 
-                        & query(dataset,"age",a)<50))
-  }
+  er.her.lymph.age.event <- c(er.her.lymph.age.event, 
+                              length(subset(huc[[dataset]][["clinical"]],
+                                            (!er & !her2 & lymph & age<50 & !event.5))[,1])
+                              )
 }
 
-ER.HER2.lymph.50 <- sum(bool, na.rm = TRUE)/length(bool)
+er.her.lymph.age.event[length(dataset.collection)+1] <- sum(er.her.lymph.age.event)
 
-Q5 <- rbind(Q5,c(rep(NA,times = length(dataset.collection)),ER.HER2.lymph.50))
-
-## h) ER- and HER2- and lymph+ and <50 years old and no event at 5 years
-bool <- c()
-for (dataset in dataset.collection){
-  for (a in 1:num.patients(dataset)){
-    bool <- c(bool, (!query(dataset,"er",a) 
-                      & !query(dataset,"her2",a) 
-                      & query(dataset,"lymph",a) 
-                      & query(dataset,"age",a)<50
-                      & !query(dataset,"event.5",a)))
-  }
-}
-
-ER.HER2.lymph.50.event5 <- sum(bool, na.rm = TRUE)/length(bool)
-
-Q5 <- rbind(Q5,c(rep(NA,times = length(dataset.collection)),ER.HER2.lymph.50.event5))
+Q5 <- rbind(Q5,er.her.lymph.age.event/number.of.patients)
 
 colnames(Q5) <- c(dataset.collection,"all")
 rownames(Q5) <- c("Number of patients", 
                   "ER+ fraction", 
-                  "Ratio of good to poor outcomes",
+                  "Ratio of good:poor outcomes",
+                  "Numeric ratio of good:poor outcomes",
                   "HER2+ fraction",
                   "ER+ and HER2+ fraction",
-                  "ER-,HER2-,lymph+,>50 fraction",
-                  "ER-,HER2-,lymph+,>50,no event.5 fraction")
+                  "ER-,HER2-,lymph+,<50 fraction",
+                  "ER-,HER2-,lymph+,<50,no event.5 fraction")
 View(Q5)
